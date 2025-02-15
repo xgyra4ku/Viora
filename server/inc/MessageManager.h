696c80f9c5265da29cfed14c0d0c55d482c45f77
KyrA-github <korenovsostav@gmail.com>
2025-02-16 02:40:38 +0300

    chat

---
+++ server/inc/MessageManager.h
@@ -0,0 +1,11 @@
+#ifndef MESSAGEMANAGER_H
+#define MESSAGEMANAGER_H
+
+class MessageManager {
+    int idHundred = 0;
+
+public:
+    bool getOneHundredMessage(int idHundred);
+};
+
+#endif //MESSAGEMANAGER_H
---
+++ server/src/MessageManager.cpp
@@ -0,0 +1,1 @@
+#include "../inc/MessageManager.h"
